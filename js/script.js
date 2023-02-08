fda9e1a5ae4d0bd3dcacb04d232a8f19ae9d61c5
Rajjit <2.0lairaj@gmail.com>
2023-02-08 16:27:28 +0530

    v11

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// counter
 function updateVisitCount(response) {
           document.getElementById('count').innerText = response.value;
 }
+
+// change the title
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+          document.title = "💔Please don't go! Come back💔";
+})
+window.addEventListener("focus", () => {
+          document.title = docTitle;
+})
